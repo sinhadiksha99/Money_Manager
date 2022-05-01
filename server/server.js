d9ec4693486c58fa236970a473b26493f31f812b
Anuj Rautela <anuj.rautela@iiitb.ac.in>
2022-05-02 01:48:14 +0530

    Mongoose Uri Updated

--- server/server.js
+++ server/server.js
@@ -12,13 +12,6 @@ connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
 });
 
-// // Connection URL
-// mongoose.Promise = global.Promise
-// mongoose.connect(config.mongoUri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
-// mongoose.connection.on('error', () => {
-//   throw new Error(`unable to connect to database: ${config.mongoUri}`)
-// })
-
 app.listen(config.port, (err) => {
   if (err) {
     console.log(err);
