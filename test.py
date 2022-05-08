58652d113ff2b394a267d4b12569b2de778586e1
Anuj Rautela <anuj.rautela@iiitb.ac.in>
2022-05-08 17:16:03 +0530

    Updated Test

--- test.py
+++ test.py
@@ -1,24 +1,24 @@
 import requests
-print('Testing this URL : http://52.90.150.143:9000//api/expenses?firstDay=2022-04-20&lastDay=2022-05-04' )
+print('Testing this URL : http://54.152.170.108:9000//api/expenses?firstDay=2022-04-20&lastDay=2022-05-04' )
 
-x = requests.get('http://52.90.150.143:9000//api/expenses?firstDay=2022-04-20&lastDay=2022-05-04')
+x = requests.get('http://54.152.170.108:9000//api/expenses?firstDay=2022-04-20&lastDay=2022-05-04')
 assert 200 == x.status_code
 print('Response Status : '  + str(x.status_code))
 
 
-print('Testing this URL : http://52.90.150.143:9000/auth/signin' )
+print('Testing this URL : http://54.152.170.108:9000/auth/signin' )
 
 myobj = {'email': 'Anuj.rautela@iiitb.ac.in', 'password' : 'anuj2014'}
 
-x = requests.post('http://52.90.150.143:9000/auth/signin', json=myobj)
+x = requests.post('http://54.152.170.108:9000/auth/signin', json=myobj)
 assert 200 == x.status_code
 print('Response Status : '  + str(x.status_code))
 
 
 
-print('Testing this URL : http://52.90.150.143:9000//api/users/' )
+print('Testing this URL : http://54.152.170.1089000//api/users/' )
 
-x = requests.get('http://52.90.150.143:9000//api/users/')
+x = requests.get('http://54.152.170.108:9000//api/users/')
 assert 200 == x.status_code
 
 print('Response Status : '  + str(x.status_code))
