4711d176001cbf0883f3e41800dad0590e1a176f
Anuj-2014 <95769742+Anuj-2014@users.noreply.github.com>
2022-05-08 18:01:19 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,14 @@
 import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+
+
+session = requests.Session()
+retry = Retry(connect=3, backoff_factor=0.5)
+adapter = HTTPAdapter(max_retries=retry)
+session.mount('http://', adapter)
+session.mount('https://', adapter)
+
 print('Testing this URL : http://172.31.31.28:9000//api/expenses?firstDay=2022-04-20&lastDay=2022-05-04' )
 
 x = requests.get('http://172.31.31.28:9000//api/expenses?firstDay=2022-04-20&lastDay=2022-05-04')
