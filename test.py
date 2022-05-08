299512b8477ca2d25adbf57b0bca2d1e29d6ad6a
Anuj Rautela <anuj.rautela@iiitb.ac.in>
2022-05-08 12:36:06 +0530

    SIGNIN API TESTING

--- test.py
+++ test.py
@@ -6,10 +6,10 @@ assert 200 == x.status_code
 print('Response Status : '  + str(x.status_code))
 
 
-print('Testing this URL : http://52.90.150.143:9000//api/expenses?firstDay=2022-04-20&lastDay=2022-05-04' )
+print('Testing this URL : http://52.90.150.143:9000//auth/signin' )
 
 myobj = {'email': 'Anuj.rautela@iiitb.ac.in', 'password' : 'anuj2014'}
 
-x = requests.post('http://52.90.150.143:9000//auth/signin', data=myobj)
+x = requests.post('http://52.90.150.143:9000/auth/signin', json=myobj)
 assert 200 == x.status_code
 print('Response Status : '  + str(x.status_code))
